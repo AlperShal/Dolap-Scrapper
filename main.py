fe49ed3bbf99e99efa1d416f959a3c03c8568b44
AlperShal <34231577+AlperShal@users.noreply.github.com>
2023-02-13 21:01:49 +0300

    Added support for multiple searches in once.
    This feature could have been implemented in a MUCH better way than this, but that would have need me to re-write lots of things. Since this is a `me project`, I didn't want to do that. If it works, it works.

--- main.py
+++ main.py
@@ -6,70 +6,109 @@ from email.utils import formatdate
 from email.mime.text import MIMEText
 
 
-url = 'https://dolap.com/ara?q=Guitar+Hero'
+url = '' # Ex: https://dolap.com/ara?q=Guitar+Hero
+url2 = ''
 headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36'}
 
-smtp_server = "mail.sal.web.tr" # Ex: mail.example.com
+smtp_server = "" # Ex: mail.example.com
 port = 587  # For TLS (STARTTLS)
-sender = 'alper@sal.web.tr' # Ex: alper@example.com
-receiver = 'alper@sal.web.tr' # Ex: alper@example.com
-password = 'nDj89^Naq^f6@j7vV%iA%6BVKycX#YKe'
+sender = '' # Ex: alper@example.com
+receiver = '' # Ex: alper@example.com
+password = ''
 context = ssl.create_default_context()
+
+
+def get_search_str(url):
+    after_q = url.split('q=')[1]
+    words = after_q.split('+')
+    search_str = ''
+    for i in words:
+        search_str = search_str + i + ' '
+    search_str = search_str.removesuffix(' ')
+    return search_str
+
+def get_listing_count(url):
+    # Get HTML
+    html = requests.get(url, headers=headers).text
+
+    # Convert to BS
+    soup = BeautifulSoup(html, 'html.parser')
+
+    # Find Count Element
+    count_element = soup.find(attrs="span", class_='subtitle')
+
+    # Get Count Element Text
+    count_element_text = str(count_element.text)
+
+    # Get Count Value, Print and Return
+    count = int(count_element_text.split()[0])
+    return count
+
+def compare_and_mail(log_file, new_value, url, search_str):
+    # Print what is being searched for
+    print(f'Session: {search_str}')
+
+    # Print new value
+    print(f'Dolap: {count}')
+
+    # Get Logged Value (From Previous Session)
+    with open(f'{log_file}', 'r') as count_log:
+        log = int(count_log.read())
+        print(f'Log: {log}')
+
+    # Check if count changed. If not, do nothing 
+    if (log == new_value):
+        print('No difference on value.')
+    # Check if count decreased. If so, log new value and do nothing.
+    elif (log > new_value):
+        print('Value decreased! Logging new value. No need for mail.')
+
+        with open(f'{log_file}', 'w') as count_log:
+            count_log.write(str(new_value))
+        print('Value logged.')
+    # Check if count increased. If so, log new value and send mail.
+    elif (log < new_value):
+        print('Value increased! Logging new value and sending mail.')
+        with open(f'{log_file}', 'w') as count_log:
+            count_log.write(str(new_value))
+
+        print('Value logged. Sending mail...')
+        msg = MIMEText(f'Listing count increased from {log} to {new_value}. URL: {url}')
+        msg['Subject'] = f'Check Dolap for {search_str}!'
+        msg['From'] = sender
+        msg['To'] = receiver
+        msg["Date"] = formatdate(localtime=True)
+
+        with smtplib.SMTP(smtp_server, port) as server:
+            server.ehlo() 
+            server.starttls(context=context)
+            server.ehlo()
+            server.login(sender, password)
+            server.sendmail(sender, receiver, msg.as_string())
+            print('Sent mail! See you tomorrow!')
+    
+    print('---------------')
+
 
 # Print date-time
 print(datetime.now())
+print('---------------')
 
-# Get HTML
-html = requests.get(url, headers=headers).text
+# Get listing count
+count = get_listing_count(url)
 
-# Convert to BS
-soup = BeautifulSoup(html, 'html.parser')
+# Get search string
+search_str = get_search_str(url)
 
-# Find Count Element
-count_element = soup.find(attrs="span", class_='subtitle')
+# Compare with log and send mail if necessary
+compare_and_mail('count_log.txt', count, url, search_str)
 
-# Get Count Element Text
-count_element_text = str(count_element.text)
+# Check if there is another URL (url2) and if so apply same steps (You can multiply this block and change vars according to your needs)
+if url2 != '':
+    count = get_listing_count(url2)
+    search_str = get_search_str(url2)
+    compare_and_mail('count_log2.txt', count, url2, search_str)
 
-# Get Count Value and Print
-count = int(count_element_text.split()[0])
-print(f'Dolap: {count}')
-
-# Get Logged Value (From Previous Session)
-with open('count_log.txt', 'r') as count_log:
-    log = int(count_log.read())
-    print(f'Log: {log}')
-
-# Check if count changed. If not, do nothing 
-if (log == count):
-    print('No difference on value.')
-# Check if count decreased. If so, log new value and do nothing.
-elif (log > count):
-    print('Value decreased! Logging new value. No need for mail.')
-
-    with open('count_log.txt', 'w') as count_log:
-        count_log.write(str(count))
-    print('Value logged.')
-# Check if count increased. If so, log new value and send mail.
-elif (log < count):
-    print('Value increased! Logging new value and sending mail.')
-    with open('count_log.txt', 'w') as count_log:
-        count_log.write(str(count))
-
-    print('Value logged. Sending mail...')
-    msg = MIMEText(f'Listing count increased from {log} to {count}. URL: {url}')
-    msg['Subject'] = 'Check Dolap!'
-    msg['From'] = sender
-    msg['To'] = receiver
-    msg["Date"] = formatdate(localtime=True)
-    
-    with smtplib.SMTP(smtp_server, port) as server:
-        server.ehlo()  # Can be omitted
-        server.starttls(context=context)
-        server.ehlo()  # Can be omitted
-        server.login(sender, password)
-        server.sendmail(sender, receiver, msg.as_string())
-        print('Sent mail! See you tomorrow!')
 
 # Seperator for Docker logs
 print('===============')
